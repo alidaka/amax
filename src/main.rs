4312a8252425083245606d1becfd8d9b1aac5d89
alidaka <alidaka@gmail.com>
2019-04-07 17:56:53 -0700

    websockets, but errors?

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,54 @@
-extern crate iron;
+extern crate ws;
 
-use iron::prelude::*;
-use iron::status;
+use serde::{Deserialize, Serialize};
+use ws::{listen, CloseCode, Handler, Message, Result, Sender};
 
-fn main() {
-    fn hello_world(_: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, "Hello World!")))
+#[derive(Serialize, Deserialize)]
+struct RawRequest {
+    data: (String, serde_json::Value),
+}
+
+/*
+#[derive(Serialize, Deserialize)]
+struct Request {}
+
+#[derive(Serialize, Deserialize)]
+struct Response {}
+*/
+
+struct Server {
+    out: Sender,
+}
+
+impl Handler for Server {
+    fn on_message(&mut self, msg: Message) -> Result<()> {
+        fn serde_to_ws(err: serde_json::error::Error) -> Result<RawRequest> {
+            Err(ws::Error::new(ws::ErrorKind::Custom(Box::new(err)), ""))
+        };
+
+        match &msg {
+            Message::Text(text) => {
+                let r = serde_json::from_str(&text).or_else(&serde_to_ws)?;
+                match r.data.0.as_ref() {
+                    "ping" => self.out.send("pong"),
+                    _ => self.out.send(msg),
+                }
+            }
+            _ => self.out.send(msg),
+        }
     }
 
-    let _server = Iron::new(hello_world).http("localhost:3000").unwrap();
-    println!("On 3000");
+    fn on_close(&mut self, code: CloseCode, reason: &str) {
+        match code {
+            CloseCode::Normal => println!("The client is done with the connection."),
+            CloseCode::Away => println!("The client is leaving the site."),
+            _ => println!("The client encountered an error: {}", reason),
+        }
+    }
+}
+
+fn main() {
+    listen("0.0.0.0:3000", |out| Server { out: out }).unwrap();
+
+    println!("We're up");
 }
