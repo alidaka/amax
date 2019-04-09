fd4276850fcbd44e9f124ade1b7a940ec0b3b3cb
alidaka <alidaka@gmail.com>
2019-04-08 21:57:04 -0700

    deserializing requests

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,19 @@ extern crate ws;
 use serde::{Deserialize, Serialize};
 use ws::{listen, CloseCode, Handler, Message, Result, Sender};
 
-#[derive(Serialize, Deserialize)]
-struct RawRequest {
-    data: (String, serde_json::Value),
+#[derive(Deserialize)]
+#[serde(untagged)]
+enum Request {
+    Ping { message: String },
+    Pong { number: i32 },
 }
 
-/*
-#[derive(Serialize, Deserialize)]
-struct Request {}
-
-#[derive(Serialize, Deserialize)]
-struct Response {}
-*/
+#[derive(Serialize)]
+#[serde(untagged)]
+enum Response {
+    Error { reason: String },
+    Success { message: String },
+}
 
 struct Server {
     out: Sender,
@@ -22,19 +23,29 @@ struct Server {
 
 impl Handler for Server {
     fn on_message(&mut self, msg: Message) -> Result<()> {
-        fn serde_to_ws(err: serde_json::error::Error) -> Result<RawRequest> {
+        fn serde_to_ws(err: serde_json::error::Error) -> Result<Request> {
             Err(ws::Error::new(ws::ErrorKind::Custom(Box::new(err)), ""))
         };
 
         match &msg {
             Message::Text(text) => {
                 let r = serde_json::from_str(&text).or_else(&serde_to_ws)?;
-                match r.data.0.as_ref() {
-                    "ping" => self.out.send("pong"),
-                    _ => self.out.send(msg),
+                match &r {
+                    Request::Ping { message: _ } => self.out.send("pong"),
+                    Request::Pong { number } => {
+                        for _ in 1..(*number) {
+                            self.out.send("ding dong")?;
+                        }
+                        self.out.send("ding dong")
+                    }
                 }
             }
-            _ => self.out.send(msg),
+            _ => self.out.send(
+                serde_json::to_string(&Response::Error {
+                    reason: "expected text, got binary".to_owned(),
+                })
+                .or_else(|err| Err(ws::Error::new(ws::ErrorKind::Custom(Box::new(err)), "")))?,
+            ),
         }
     }
 
@@ -42,6 +53,7 @@ impl Handler for Server {
         match code {
             CloseCode::Normal => println!("The client is done with the connection."),
             CloseCode::Away => println!("The client is leaving the site."),
+            CloseCode::Status => println!("The client has left with no status code."),
             _ => println!("The client encountered an error: {}", reason),
         }
     }
